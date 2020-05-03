9d0069dc2af5eadc1161601377e2f6a7b73a9c63
Michael Dorst <m@mdorst.net>
2020-05-03 02:26:18 -0700

    Complete

--- public/javascripts/queue.js
+++ public/javascripts/queue.js
@@ -1,7 +1,7 @@
 'use strict'
 
 /* Direct ESLint to ignore the following undefined (as far as it can tell) variable(s): */
-/* global Hammer, userfname, userlname, users, horses */
+/* global Hammer, horses, user, users */
 
 /**
  * Shorten the user's name based on the width of the container it will be displayed in.
@@ -65,6 +65,26 @@ showAssignedFarriers()
 
 $(window).resize(showAssignedFarriers)
 
+/**
+ * Filters the queue to show only the horses assigned to the current user.
+ * Uses a custom DataTable search to filter down the list.
+ */
+function filterAssigned() {
+  // In order to filter by "Assigned To Me", we effectively do a custom search for all horses
+  // assigned to the logged in user. queueRow[3] is the part of the table row that containst
+  // the horse's id.
+  $.fn.dataTable.ext.search.push((_, __, ___, queueRow) => {
+    // queueRow[3] is the raw html text of the "Assigned To" column of the current row.
+    // Extract the horse's id number from the html:
+    const horseId = Number(queueRow[3].match(/id="([0-9]+)"/)[1])
+    // Get the horse with that id.
+    const horse = horses.find(horse => horseId === horse.id)
+    // Return true if this horse is assigned to the logged in user.
+    // (Returning true adds this row to the "search results").
+    return user._id === horse.assignedFarrier
+  })
+}
+
 $(document).ready(function() {
   var table = $('#queue').DataTable({
     responsive: {
@@ -83,11 +103,7 @@ $(document).ready(function() {
     dom: '<"row"<"col"<"checkbox-area">><"col"fr>>tilpr',
     initComplete: function() {
       if (localStorage.input === 'true') {
-        $.fn.dataTable.ext.search.push(function(_, data) {
-          return (
-            data[3].trim() === userfname.innerText.charAt(0) + '.' + userlname.innerText.charAt(0)
-          )
-        })
+        filterAssigned()
       }
     }
   })
@@ -100,11 +116,7 @@ $(document).ready(function() {
 
   $('#view-assigned').on('change', function() {
     if ($(this).is(':checked')) {
-      $.fn.dataTable.ext.search.push(function(_, data) {
-        return (
-          data[3].trim() === userfname.innerText.charAt(0) + '.' + userlname.innerText.charAt(0)
-        )
-      })
+      filterAssigned()
     } else {
       $.fn.dataTable.ext.search.pop()
     }
